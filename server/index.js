603f1d1f0aa485559056ffc2acc1ef387b6e1f1f
Marian <mlukavyi@gmail.com>
2021-05-24 15:10:33 +0300

    Websocket

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http,{
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+      }
+})
+
+io.on('connection', socket => {
+    socket.on('message', ({name, message}) => {
+        io.emit('message', {name,message})
+    })
+})
+
+http.listen(4000, () => {
+    console.log('Server is running on port 4000')
+})
